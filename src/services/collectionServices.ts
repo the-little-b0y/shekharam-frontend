d9d76a9f644cfec8a09addbff049c2487e99f4e8
Ritwick <32520220+the-little-b0y@users.noreply.github.com>
2022-11-12 12:56:12 +0530

    feat: Added Search Collection, Copy (#10)

--- src/services/collectionServices.ts
+++ src/services/collectionServices.ts
@@ -1,9 +1,11 @@
 import axios from './index'
 import { ApiResponse } from '../contracts/axiosInterface';
-import { CollectionInterface, GetCollectionReturnInterface, GetSingleCollectionReturnInterface } from '../contracts/collectionInterface';
+import { CollectionInterface, GetCollectionIdByCopyQRReturnInterface, GetCollectionIdBySetQRReturnInterface, GetCollectionReturnInterface, GetSingleCollectionReturnInterface } from '../contracts/collectionInterface';
 
 const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
 const COLLECTIONSERVICE_ROUTE = `${BACKEND_API_URL}/collection`
+const COLLECTIONSETQRSERVICE_ROUTE = `${COLLECTIONSERVICE_ROUTE}/collectionsetqr`
+const COPYQRSERVICE_ROUTE = `${COLLECTIONSERVICE_ROUTE}/copyqr`
 
 export const postCollection = async (collection: CollectionInterface) => {
     const response = await axios.post<ApiResponse>(COLLECTIONSERVICE_ROUTE, collection)
@@ -17,6 +19,16 @@ export const getCollection = async (collectionof: string) => {
 
 export const getSingleCollection = async (id: string) => {
     const response = await axios.get<GetSingleCollectionReturnInterface>(`${COLLECTIONSERVICE_ROUTE}/${id}`)
+    return response.data
+}
+
+export const getCollectionIdBySetQR = async (collectionsetqr: string) => {
+    const response = await axios.get<GetCollectionIdBySetQRReturnInterface>(COLLECTIONSETQRSERVICE_ROUTE, {params: {collectionsetqr: collectionsetqr}})
+    return response.data
+}
+
+export const getCollectionIdByCopyQR = async (copyqr: string) => {
+    const response = await axios.get<GetCollectionIdByCopyQRReturnInterface>(COPYQRSERVICE_ROUTE, {params: {copyqr: copyqr}})
     return response.data
 }
 
